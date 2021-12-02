b37580fe5a6d2861197e1d3a8cbf25149967c89b
ckayoux <felix.houdebert@utt.fr>
2021-12-02 23:27:38 +0100

    zzz

--- scripts/lib/create.sh
+++ scripts/lib/create.sh
@@ -2,7 +2,6 @@
 LIB_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PATHPARSER="${LIB_DIR}/parsepath.sh"
 ARCHIVES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../archives" &> /dev/null && pwd )"
-
 USAGE="Usage : `basename $0` [archive-path]"
 
 if test $# -gt 1
@@ -14,16 +13,13 @@ if test $# -eq 0
 then
     set "${ARCHIVES_DIR}/.created_archive"
 fi
+OLDIFS=$IFS
 ARCHIVE="$1"
+HEADER="/tmp/.temp_header"
 HEADERLEN=`expr "$(find . |wc -l)" + 2 \* "$(find -type d |wc -l)" - 2`
 HEADERSTART=3
 BODYSTART=`expr "$HEADERLEN" + "$HEADERSTART" + 1`
-exec > "$ARCHIVE" #les echo écriront dans le fichier $ARCHIVE
-echo "$HEADERSTART:$BODYSTART"
-for (( i = 2; i < $HEADERSTART ; i++ ))
-do
-    echo ""
-done
+exec > "$HEADER" #les echo écriront dans le fichier $ARCHIVE
 R=`basename "$(pwd)"` #racine de l'archive
 dirsList=`find . -type d` &> /dev/null #récupération des répertoires
 i=0
@@ -33,32 +29,43 @@ do
     echo "directory $directory" #début du dossier
     while IFS= read -r f
     do  
+        if test -z "$f"
+        then
+            break
+        fi
+
         IFS=' '
         #récupération des droits, de la taille et du nom de chaque élément directement contenu dans le dossier
-        
         read frights fsize <<< "$(echo $f |cut -d" " -f1,5)"
         fname=`echo "$f" |awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print s }'`
         echo "$fname $frights $fsize"
-    done <<<  "$(ls -l $d |tail +2)"
+    done <<<  "$(ls -al $d |tail +4)"
     (( i++ ))
     if test $i -lt $HEADERLEN; then echo "@" #fin du dossier
     fi
 done
+exec > "$ARCHIVE"
+echo "$HEADERSTART:$BODYSTART"
+for (( i = 2; i < $HEADERSTART ; i++ ))
+do
+    echo ""
+done
+while IFS= read -r line
+do
+    printf "%s\n" "$line"
+done <"$HEADER"
+
 fpos=$HEADERSTART
 fcontentstart=$BODYSTART
 cd "$(pwd)"
 while IFS= read -r line
 do
+    printf "$line" >> "/home/f/Bureau/createlog"
     if [ -n "$(echo "$line" |egrep -- "\-[rwx\-]{9} [0-9]+\$")" ]
     then
         fname=`echo "$line"|awk '{ s = "" ; for (i = 1; i <= NF - 2; i++) s = s $i " "; print s}'`
         exec >> "$ARCHIVE"
-        fcontent=""
-        while read -r line
-        do
-            fcontent="$fcontent""$line"
-        done < "$("$PATHPARSER" toFS "$d""$fname")"
-        echo "$d""$fname" > "/home/f/Bureau/createlog"
+        fcontent=`cat "$("$PATHPARSER" toFS "$d""$fname")"`
         if test -z "$fcontent"
         then
             fcontentlen=0
@@ -73,4 +80,5 @@ do
         d=`echo "$line" |sed "s/directory \(.*\)$/\1/"`
     fi
     (( fpos++ ))
-done <<< $HEADER
+done < "$HEADER"
+rm "$HEADER"
--- scripts/vsh.sh
+++ scripts/vsh.sh
@@ -6,16 +6,16 @@ VSH_SERVER="${SCRIPTDIR}vsh-server.sh"
 VSH_CLI="${SCRIPTDIR}vsh-cli.sh"
 
 parse-args () {
-	CMD=$1
+	CMD=`echo "$1" |sed 's/-\?\([a-zA-Z]\)/\1/'` #transforms -cmd into cmd if there is a '-' 
 	shift
 	ARGS=$*
 
 	case $CMD in
-	    	'listen')
+	    '-listen' | 'listen' )
 			PORT=$1
 			"$VSH_SERVER" $PORT;;
 
-		'list' | 'create' | 'browse' | 'extract' )
+		'list' | 'create' | 'browse' | 'extract')
 			"$VSH_CLI" $CMD "$ARGS";;
 		
 
